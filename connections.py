2b3090ae908025e593881c80096c1a1d02220f5a
divijakinger <divija.kinger@somaiya.edu>
2022-09-18 20:35:20 +0530

    url

--- connections.py
+++ connections.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-DB_URL = 'postgres://ktuubilltlnyrg:1b70da51ac2094134cfddb59cbedbe66bce37a11a2aab82e468b6c813ff2034a@ec2-44-209-158-64.compute-1.amazonaws.com:5432/d56g97aa7iciem'
+DB_URL = 'postgresql://ktuubilltlnyrg:1b70da51ac2094134cfddb59cbedbe66bce37a11a2aab82e468b6c813ff2034a@ec2-44-209-158-64.compute-1.amazonaws.com:5432/d56g97aa7iciem'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning

7ebaebda004008e58bcbced31445cc2d03e47ecf
divijakinger <divija.kinger@somaiya.edu>
2022-09-24 13:49:47 +0530

    try

--- app.py
+++ app.py
@@ -8,12 +8,6 @@ from flask_session import Session
 from flask_cors import CORS, cross_origin
 import os
 
-app.config['SECRET_KEY'] = os.urandom(24)
-app.config['SESSION_COOKIE_NAME'] = "my_session"
-app.config["SESSION_PERMANENT"] = True
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-CORS(app)
 
 @app.route('/',methods=['GET'])
 def home():
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,14 @@
 from app import app
+from flask import session
+from flask_session import Session
+from flask_cors import CORS, cross_origin
+import os
+
 if "__name__" == "__main__":
+    app.config['SECRET_KEY'] = os.urandom(24)
+    app.config['SESSION_COOKIE_NAME'] = "my_session"
+    app.config["SESSION_PERMANENT"] = True
+    app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
+    CORS(app)
     app.run()

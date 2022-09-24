a34932944b954787b82b281a1f07bf8385a14562
divijakinger <divija.kinger@somaiya.edu>
2022-09-24 13:40:44 +0530

    try

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from classes.connections import *
 from classes.people import *
 from flask_session import Session
 from flask_cors import CORS, cross_origin
+import os
 
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SESSION_COOKIE_NAME'] = "my_session"
 app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)

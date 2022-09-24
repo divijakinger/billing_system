f6e214413db5770e3b86280b087903d6e54e5bdd
divijakinger <divija.kinger@somaiya.edu>
2022-09-24 13:07:19 +0530

    testing

--- testing.py
+++ testing.py
@@ -1,3 +1,3 @@
 import requests
-
 response = requests.post("https://mibillingsystem.herokuapp.com/login",json={'phone':9231456879,'password':'Admin1'})
+response = requests.get("https://mibillingsystem.herokuapp.com/getDetails")
